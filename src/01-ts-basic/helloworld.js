033f18b1f0f96fae14a93eaf1f758c954e4f51b5
zhangheng <zhangheng@massmeet.cn>
2023-02-04 10:59:05 +0800

    create tsconfig.json and test demo

---
+++ src/01-ts-basic/helloworld.js
@@ -0,0 +1,4 @@
+function greet(name, date) {
+  console.log("Hello ".concat(name, ", today is ").concat(date));
+}
+greet('Edison', '2023-02-04');
---
+++ src/01-ts-basic/helloworld.ts
@@ -0,0 +1,5 @@
+function greet(name: string, date: string) {
+  console.log(`Hello ${name}, today is ${date}`);
+}
+
+greet('Edison', '2023-02-04');
